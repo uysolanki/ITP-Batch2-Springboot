3d42b09ca52cf7ec78a458ece367256dfcc6eb4a
Umar Solanki <uysolanki@gmail.com>
2025-05-06 12:51:57 +0530

    request body

--- src/main/java/com/itp/alibaba/controller/ProductController.java
+++ src/main/java/com/itp/alibaba/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.itp.alibaba.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,45 @@ public class ProductController {
 		product.setPrice(prodPrice);
 		return productService.addProduct(product);
 		//return "Product Added Successfully";
+		
+	}
+	
+	@PostMapping("/addProductByPathVariable/{a}/{b}/{c}/{d}")		//insert post mapping
+	public Product addProductByPathVariable(@PathVariable("a") String prodTitle,
+			@PathVariable("b") String prodDesc,
+			@PathVariable("c") String prodCategory,
+			@PathVariable("d") double prodPrice)
+	{
+		Product product=new Product();
+		product.setProductTitle(prodTitle);
+		product.setProductDesc(prodDesc);
+		product.setProductCategory(prodCategory);
+		product.setPrice(prodPrice);
+		return productService.addProduct(product);
+		//return "Product Added Successfully";
+		
+	}
+	
+	@PostMapping("/addProductByPathVariable2/{prodTitle}/{prodDesc}/{prodCategory}/{prodPrice}")		//insert post mapping
+	public Product addProductByPathVariable2(@PathVariable String prodTitle,
+			@PathVariable String prodDesc,
+			@PathVariable String prodCategory,
+			@PathVariable double prodPrice)
+	{
+		Product product=new Product();
+		product.setProductTitle(prodTitle);
+		product.setProductDesc(prodDesc);
+		product.setProductCategory(prodCategory);
+		product.setPrice(prodPrice);
+		return productService.addProduct(product);
+		//return "Product Added Successfully";
+		
+	}
+	
+	@PostMapping("/addProductByRequestBody")		//insert post mapping
+	public Product addProductByRequestBody(@RequestBody Product product)
+	{
+		return productService.addProduct(product);
 		
 	}
 }
